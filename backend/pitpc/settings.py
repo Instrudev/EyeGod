df2cbab734b7e2bf936237d57b6b42c716cfc5b8
Instrudev <167267350+Instrudev@users.noreply.github.com>
2025-12-19 12:03:55 -0500

    Update settings.py

--- backend/pitpc/settings.py
+++ backend/pitpc/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False),
-    SECRET_KEY=(str, "change-me"),
+    SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key"),
     ALLOWED_HOSTS=(list, ["*"]),
     DB_NAME=(str, "u442853790_infinitygo"),
     DB_USER=(str, "u442853790_infinitygo"),
